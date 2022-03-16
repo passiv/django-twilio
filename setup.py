0d050c9c4190d9f5179ffb08c52357198da5b7ae
Nick McCullum <nicholasmccullum@gmail.com>
2022-03-16 19:08:05 -0400

    fix version

--- setup.py
+++ setup.py
@@ -11,55 +11,52 @@ INSTALL_PYTHON_REQUIRES = []
 # For the official support, please visit:
 # https://docs.djangoproject.com/en/3.2/faq/install/#what-python-version-can-i-use-with-django and you may change the version in the URL to suit your needs, and we will try to update that here too as we upgrade with django.
 if sys.version_info[1] == 5:
-    django_python_version_install = 'Django>=2.2,<3.0',
+    django_python_version_install = ("Django>=2.2,<3.0",)
     INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
 elif sys.version_info[1] == 6:
-    django_python_version_install = 'Django>=2.2,<3.3',
+    django_python_version_install = ("Django>=2.2,<3.3",)
     INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
 elif sys.version_info[1] == 7:
-    django_python_version_install = 'Django>=2.2,<3.3'
+    django_python_version_install = "Django>=2.2,<3.3"
     INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
 elif sys.version_info[1] == 8:
-    django_python_version_install = 'Django>=2.2.8,<3.3'
+    django_python_version_install = "Django>=2.2.8,<3.3"
     INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
 elif sys.version_info[1] == 9:
     # slightly too broad (3.0.11 for v3.0, and 3.1.3 for v3.1) -- may need to fix
-    django_python_version_install = 'Django>=2.2.17,<4.1'
+    django_python_version_install = "Django>=2.2.17,<4.1"
     INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
 elif sys.version_info[1] == 10:
-    django_python_version_install = 'Django>=3.2.9,<4.0'
+    django_python_version_install = "Django>=3.2.9,<4.1"
     INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
 
 setup(
-
     # Basic package information:
-    name='django-twilio',
-    version='0.13.2',
+    name="django-twilio",
+    version="0.13.2",
     packages=find_packages(),
-
     # Packaging options:
     zip_safe=False,
     include_package_data=True,
-
     # Package dependencies:
     install_requires=[
-        'setuptools>=36.2',
-        'twilio>=7,<8',
-        'django-phonenumber-field>=0.6',
-        'phonenumbers>=8.10.22',
-    ] + INSTALL_PYTHON_REQUIRES,
-
+        "setuptools>=36.2",
+        "twilio>=7,<8",
+        "django-phonenumber-field>=0.6",
+        "phonenumbers>=8.10.22",
+    ]
+    + INSTALL_PYTHON_REQUIRES,
     # Metadata for PyPI:
-    author='Randall Degges',
-    author_email='rdegges@gmail.com',
+    author="Randall Degges",
+    author_email="rdegges@gmail.com",
     maintainer="Jason Held",
     maintainer_email="jasonsheld@gmail.com",
-    license='UNLICENSE',
-    url='https://github.com/rdegges/django-twilio',
-    keywords='twilio telephony call phone voip sms django django-twilio',
-    description='Build Twilio functionality into your Django apps.',
+    license="UNLICENSE",
+    url="https://github.com/rdegges/django-twilio",
+    keywords="twilio telephony call phone voip sms django django-twilio",
+    description="Build Twilio functionality into your Django apps.",
     long_description=open(
-        normpath(join(dirname(abspath(__file__)), 'README.rst'))
+        normpath(join(dirname(abspath(__file__)), "README.rst"))
     ).read(),
     project_urls={
         "Documentation": "https://django-twilio.readthedocs.io/en/latest/",
@@ -67,25 +64,24 @@ setup(
         "Tracker": "https://github.com/rdegges/django-twilio/issues",
     },
     classifiers=[
-        'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 4.0',
-        'Intended Audience :: Developers',
-        'License :: Public Domain',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: WWW/HTTP',
-    ]
-
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 4.0",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
 )
